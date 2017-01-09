2188517c4c34acf699b96d5f9445915a877949ae
jakeiwen <iwenx006@umn.edu>
2017-01-09 10:37:01 -0600

    nav

--- public/scripts/controllers/dashboard.controller.js
+++ public/scripts/controllers/dashboard.controller.js
@@ -3,4 +3,17 @@ app.controller('DashboardController', ["$firebaseAuth", "$http", function($fireb
   var auth = $firebaseAuth();
   var currentUser = {};
 
+  self.logIn = function(){
+    auth.$signInWithPopup("google").then(function(firebaseUser) {
+      console.log("Firebase Authenticated as: ", firebaseUser.user.displayName);
+    }).catch(function(error) {
+      console.log("Authentication failed: ", error);
+    });
+  };
+  self.logOut = function(){
+    auth.$signOut().then(function(){
+      console.log('Logging the user out!');
+    });
+  }
+
 }]);
