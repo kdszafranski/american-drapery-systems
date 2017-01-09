4c07eefdb41cd058f6a04f7c90c1771cd93b412b
jakeiwen <iwenx006@umn.edu>
2017-01-09 11:19:48 -0600

    test data

--- public/scripts/controllers/dashboard.controller.js
+++ public/scripts/controllers/dashboard.controller.js
@@ -2,6 +2,32 @@ app.controller('DashboardController', ["$firebaseAuth", "$http", function($fireb
   var self = this;
   var auth = $firebaseAuth();
   var currentUser = {};
+  self.testData = [
+    {
+      survey_number: 2134,
+      job_number: 54634,
+      survey_date: "04/23/2014",
+      status: "In Progress",
+      client_name: "ACME",
+      last_modified: "04/24/2014"
+    },
+    {
+      survey_number: 2135,
+      job_number: 54637,
+      survey_date: "02/23/2014",
+      status: "Completed",
+      client_name: "DogCo",
+      last_modified: "02/29/2014"
+    },
+    {
+      survey_number: 2139,
+      job_number: 54640,
+      survey_date: "03/23/2014",
+      status: "Declined",
+      client_name: "CatCo",
+      last_modified: "03/29/2014"
+    }
+  ]
 
   self.logIn = function(){
     auth.$signInWithPopup("google").then(function(firebaseUser) {
@@ -15,5 +41,6 @@ app.controller('DashboardController', ["$firebaseAuth", "$http", function($fireb
       console.log('Logging the user out!');
     });
   }
+
 
 }]);
