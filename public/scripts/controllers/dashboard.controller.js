a3d7951e0b54fb103d3cbf240b9f2911edfaafdd
jakeiwen <iwenx006@umn.edu>
2017-01-09 10:23:34 -0600

    update dev to master

--- public/scripts/controllers/dashboard.controller.js
+++ public/scripts/controllers/dashboard.controller.js
@@ -1,6 +1,19 @@
 app.controller('DashboardController', ["$firebaseAuth", "$http", function($firebaseAuth, $http) {
   var self = this;
-  var auth = $firebaseAuth();
+  //var auth = $firebaseAuth();
   var currentUser = {};
+
+  // self.logIn = function(){
+  //   auth.$signInWithPopup("google").then(function(firebaseUser) {
+  //     console.log("Firebase Authenticated as: ", firebaseUser.user.displayName);
+  //   }).catch(function(error) {
+  //     console.log("Authentication failed: ", error);
+  //   });
+  // };
+  // self.logOut = function(){
+  //   auth.$signOut().then(function(){
+  //     console.log('Logging the user out!');
+  //   });
+  // }
 
 }]);
