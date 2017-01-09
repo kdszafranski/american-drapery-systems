16f65bf3c158e771b57f68c8b0fad1987bd2d2b4
Jeff Kealy <jeffkealy@yahoo.com>
2017-01-09 13:26:56 -0600

    added nav buttons and some styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'firebase',]);
+var app = angular.module('app', ['ngRoute', 'firebase', 'ngMaterial']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
---
+++ public/scripts/controllers/nav.controller.js
@@ -0,0 +1,7 @@
+app.controller('NavController', ["$firebaseAuth", "$http", function($firebaseAuth, $http) {
+  var self = this;
+  //var auth = $firebaseAuth();
+  var currentUser = {};
+
+
+}]);
