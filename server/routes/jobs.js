023f2c53c82a39462a8db3932531cf56bf194a53
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2017-01-09 10:52:12 -0600

    added spaces to the e nd of query lines. Included survey_id as parameter to queries.

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -5,15 +5,15 @@ var connectionString = 'postgres://localhost:5432/american-drapery-systems';
 
 
 //Get request to display all jobs on dashboard
-router.get('/', function(req, res) {
-  console.log('reached get jobs route')
+router.get('/:survey_id', function(req, res) {
+  console.log('reached get jobs route');
   pg.connect(connectionString, function(err, client, done) {
     if(err) {
       console.log('connection error: ', err);
       res.sendStatus(500);
     }
 
-    client.query('SELECT * FROM survey' +
+    client.query('SELECT * FROM survey ' +
     'JOIN client on survey.client_id = client.id', function(err, result) {
       done(); // close the connection.
 
@@ -28,18 +28,21 @@ router.get('/', function(req, res) {
 });
 
 //Get request to fetch specified job information(Measurements, client information and images)
-router.get('/', function(req, res) {
+router.get('/:survey_id', function(req, res) {
   console.log('reached get all jobs route route')
+  console.log(req.params.survey_id);
+  let survey_id = req.params.survey_id;
   pg.connect(connectionString, function(err, client, done) {
     if(err) {
       console.log('connection error: ', err);
       res.sendStatus(500);
     }
 
-    client.query('SELECT * FROM measurements' +
-    'JOIN survey on measurements.survey_id = survey.id' +
-    'JOIN client on survey.client_id = client.id' +
-    'WHERE survey_id = 2',
+    client.query('SELECT * FROM measurements ' +
+    'JOIN survey on measurements.survey_id = survey.id ' +
+    'JOIN client on survey.client_id = client.id ' +
+    'WHERE survey_id = ' + survey_id +
+    ' ORDER BY area',
     function(err, result) {
       done(); // close the connection.
 
--- server/routes/measurements.js
+++ server/routes/measurements.js
@@ -5,9 +5,10 @@ var connectionString = 'postgres://localhost:5432/american-drapery-systems';
 
 
 //Post request to add measurement information to job
-router.post('/', function(req,res) {
+router.post('/:survey_id', function(req,res) {
   console.log(req.body);
-  var newMeasurement = req.body;
+  let newMeasurement = req.body;
+  let survey_id = req.params.survey_id;
   pg.connect(connectionString, function(err, client, done) {
     if(err) {
       console.log('connection error: ', err);
@@ -15,7 +16,7 @@ router.post('/', function(req,res) {
     }
     client.query("INSERT INTO measurements (area, floor, room, quantity, width, length, inside, outside, fascia_size, controls, mount, fabric, notes, survey_id) " +
     "VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)",
-    [newMeasurement.area, newMeasurement.floor, newMeasurement.room, newMeasurement.quantity, newMeasurement.width, newMeasurement.length, newMeasurement.inside, newMeasurement.outside, newMeasurement.fascia_size, newMeasurement.controls, newMeasurement.mount, newMeasurement.fabric, newMeasurement.notes, newMeasurement.survey_id],
+    [newMeasurement.area, newMeasurement.floor, newMeasurement.room, newMeasurement.quantity, newMeasurement.width, newMeasurement.length, newMeasurement.inside, newMeasurement.outside, newMeasurement.fascia_size, newMeasurement.controls, newMeasurement.mount, newMeasurement.fabric, newMeasurement.notes, survey_id],
     function(err, result) {
       done(); // close the connection.
 
