11bf434fbeccc39960e2470c99bf2a4399bc2ab7
Jeff Kealy <jeffkealy@yahoo.com>
2017-01-10 16:23:36 -0600

    profile page functional

--- public/scripts/controllers/profile.controller.js
+++ public/scripts/controllers/profile.controller.js
@@ -2,8 +2,13 @@ app.controller('ProfileController', ["$http", "UserFactory", function($http, Use
   var self = this;
   self.currentProfile = {};
   self.checkbox = true;
+  self.clients = [];
+  self.selected = {};
+  self.showCompany = false;
+  self.showUpdateButton = true;
+  self.showSubmitButton = false;
 
-
+  //Submit button function
   self.submitButton = function(){
     if (self.checkbox == true) {
       self.currentProfile.billing_address_street = self.currentProfile.survey_address_street
@@ -12,61 +17,113 @@ app.controller('ProfileController', ["$http", "UserFactory", function($http, Use
       self.currentProfile.billing_address_zip = self.currentProfile.survey_address_zip
     }
     console.log("submit button clicked. Object:", self.currentProfile);
-    postClients();
+    //postClients();
   }
-  self.copyAddress = function(){
-    console.log("checkbox");
 
+  //Update existing client information Button
+  self.updateButton = function(){
+    if (self.checkbox == true) {
+      self.currentProfile.billing_address_street = self.currentProfile.survey_address_street
+      self.currentProfile.billing_address_city = self.currentProfile.survey_address_city
+      self.currentProfile.billing_address_state = self.currentProfile.survey_address_state
+      self.currentProfile.billing_address_zip = self.currentProfile.survey_address_zip
+    }
+    console.log("self.selected", self.selected);
+    updateClient();
   }
+
+  //GET client_name and id for Dropdown
   function getClients() {
-    currentUser = UserFactory.getUser();
-    console.log('getting surveys', currentUser.credential.idToken);
-    $http({
-      method: 'GET',
-      url: '/clients',
-      headers: {
-        id_token: currentUser.credential.idToken
-      }
-    }).then(function(response){
-      console.log('success');
-      formatData(response.data);
-    });
-  }
-
-  function getSurveys(){
-    currentUser = UserFactory.getUser();
-    console.log('getting surveys - currentUser:', currentUser);
-    currentUser.user.getToken().then(function(idToken) {
+    //currentUser = UserFactory.getUser();
+    //console.log("current user: ", currentUser);
+    //currentUser.user.getToken().then(function(idToken) {
       $http({
         method: 'GET',
-        url: '/surveys/all/',
-        headers: {
-          id_token: currentUser.credential.idToken
-        }
+        url: '/clients'
+        // headers: {
+        //   id_token: idToken
+        // }
       }).then(function(response){
-        console.log('success');
-        formatData(response.data);
+        console.log('success. Response', response.data);
+        self.clients = response.data
       });
-    });
+    // });
   }
 
-  function postClients() {
-    currentUser = UserFactory.getUser();
-    console.log("current user: ", currentUser);
-    console.log('getting surveys', currentUser.credential.idToken);
+  getClients();
 
-    currentUser.user.getToken().then(function(idToken) {
+  //GET client information from client selected from dropdown
+  function getClient() {
+    //currentUser = UserFactory.getUser();
+    //console.log("current user: ", currentUser);
+    //currentUser.user.getToken().then(function(idToken) {
+      $http({
+        method: 'GET',
+        url: '/clients/' + self.selected.id
+        // headers: {
+        //   id_token: idToken
+        // }
+      }).then(function(response){
+        console.log('success. Response', response.data);
+        self.currentProfile = response.data[0]
+        if (self.currentProfile.client_name == self.selected.client_name) {
+          self.showCompany = true;
+        } else {
+          self.showCompany = false;
+        }
+      });
+    // });
+  }
+
+  //POST client information to database
+  function postClients() {
+    // currentUser = UserFactory.getUser();
+    // console.log("current user: ", currentUser);
+    // currentUser.user.getToken().then(function(idToken) {
       $http({
         method: 'POST',
         url: '/clients',
-        data: self.currentProfile,
-        headers: {
-          id_token: currentUser.credential.idToken
-        }
+        data: self.currentProfile
+        // headers: {
+        //   id_token: idToken
+        // }
       }).then(function(response){
         console.log('success');
       });
-    });
+    // });
+  }
+
+  //Update client information in database
+  function updateClient() {
+    // currentUser = UserFactory.getUser();
+    // console.log("current user: ", currentUser);
+    // currentUser.user.getToken().then(function(idToken) {
+      $http({
+        method: 'POST',
+        url: '/clients/' + self.selected.id,
+        data: self.currentProfile
+        // headers: {
+        //   id_token: idToken
+        // }
+      }).then(function(response){
+        console.log('success');
+      });
+    // });
+  }
+
+  //course of action from drop down selection
+  self.dropdownOption = function(){
+    console.log("selected", self.selected);
+    if (self.selected === "None") {
+      self.currentProfile = {}
+      self.showCompany = false;
+      self.showUpdateButton = true;
+      self.showSubmitButton = false;
+    } else {
+      self.showUpdateButton = false;
+      self.showSubmitButton = true;
+      getClient();
+    }
   }
 
 
