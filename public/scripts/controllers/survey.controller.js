5016a2eeae19ad2e74f4329daffba6210eb179c0
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2017-01-10 14:37:26 -0600

    alt-contact only showing on survey view if it exists

--- public/scripts/controllers/survey.controller.js
+++ public/scripts/controllers/survey.controller.js
@@ -9,13 +9,29 @@ app.controller('SurveyController', ["$http", function($http) {
     }).then(function(response){
       self.surveyDetails = response;
       console.log("Response From Server: ", self.surveyDetails.data);
+
+      //Assigning Contact Information to variables in controller
+      self.primary_contact_name = self.surveyDetails.data[0].primary_contact_name;
+      self.primary_contact_phone_number = self.surveyDetails.data[0].primary_contact_phone_number;
+      self.primary_contact_email = self.surveyDetails.data[0].primary_contact_email;
+
+      //Only show alt contact if one exists
+      if (self.surveyDetails.data[0].alt_contact_name !== null) {
+        self.showAltContact = true;
+        self.alt_contact_name = self.surveyDetails.data[0].alt_contact_name;
+        self.alt_phone_number = self.surveyDetails.data[0].alt_phone_number;
+        self.alt_contact_email = self.surveyDetails.data[0].alt_contact_email;
+      } else {
+        self.showAltContact = false;
+      }
+
+      //Seperate measurements into areas
       var separateAreas = groupBy(self.surveyDetails.data, 'area');
       console.log(separateAreas);
       self.areaArray = [];
       for (x in separateAreas) {
         self.areaArray.push(separateAreas[x]);
       }
-      console.log(self.areaArray[2][0].area]);
     },
     function(err) {
       console.log("error getting survey details: ", err);
