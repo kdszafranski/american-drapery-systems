f078d4cebc8234d47e5f6cd80587e4c63a80ed15
Jeff Kealy <jeffkealy@yahoo.com>
2017-01-11 17:16:59 -0600

    added measurement inputs

--- public/scripts/controllers/measurement.controller.js
+++ public/scripts/controllers/measurement.controller.js
@@ -1,6 +1,11 @@
 app.controller('MeasurementController', ["$http", function($http) {
   var self = this;
+  self.measurement = {};
 
+
+  self.addButton = function(){
+    console.log("mesurement: ", self.measurement);
+  }
 
 
 }]);
