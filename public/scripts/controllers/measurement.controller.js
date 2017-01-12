5a316dde1c422b90c84cf5ee36eb148366d6eb41
Jeff Kealy <jeffkealy@yahoo.com>
2017-01-12 10:08:29 -0600

    in the middel of getting measurements to add to object

--- public/scripts/controllers/measurement.controller.js
+++ public/scripts/controllers/measurement.controller.js
@@ -1,11 +1,30 @@
 app.controller('MeasurementController', ["$http", function($http) {
   var self = this;
   self.measurement = {};
+  self.measurements =[];
 
 
   self.addButton = function(){
     console.log("mesurement: ", self.measurement);
+    self.measurements.push(self.measurement)
+    console.log("mesurement array", self.measurements);
   }
 
+  // function getMeasurements(){
+  //   currentUser = UserFactory.getUser();
+  //   console.log("current user: ", currentUser);
+  //   currentUser.user.getToken().then(function(idToken) {
+  //     $http({
+  //       method: 'GET',
+  //       url: '/clients',
+  //       headers: {
+  //         id_token: idToken
+  //       }
+  //     }).then(function(response){
+  //       console.log('success. Response', response.data);
+  //       self.clients = response.data
+  //     });
+  //   });
+  // }
 
 }]);
