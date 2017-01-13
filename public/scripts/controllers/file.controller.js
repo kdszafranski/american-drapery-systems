250c6df11a909dcbf37436a016f55ee8d6160d1e
Casey Hyde <hyde.casey@gmail.com>
2017-01-13 15:50:40 -0600

    File upload sends to aws.s3

--- public/scripts/controllers/file.controller.js
+++ public/scripts/controllers/file.controller.js
@@ -3,21 +3,32 @@ Create file controller
 ***********************/
 app.controller('FileController', ['FileFactory', '$compile',
 function(FileFactory, $compile) {
+  console.log("File controller running");
   const self = this;
 
   self.filesObject = { //store files and info here
     files: FileList,
-    filesInfo: []
+    filesInfo: {}
   };
 
   self.infoCounter = 0;
+
+  self.clickTest = function() {
+    console.log("click test clicked");
+    console.log(self.filesObject.filesInfo);
+  }
 
   self.submitFiles = function() {
     console.log('submit files clicked');
     console.log('sending these files to FileFactory: ', self.filesObject.files);
     console.log('sending this file info to FileFactory: ', self.filesObject.filesInfo);
     FileFactory.updateFiles(self.filesObject); //send filesObject to FileFactory
-    // FileFactory.submitFiles(); //send files and info to server
+    FileFactory.submitFiles(); //send files and info to server
   }
 
 }]);//End controller
+// .directive('fileInfo', function() {
+//   return {
+//     template: 'This is data from the file controller: {{files.filesObject.test}}'
+//   }
+// });
