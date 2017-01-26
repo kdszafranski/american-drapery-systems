11eda1d1b09c83247b89323495c8b42254ac41ed
Casey Hyde <hyde.casey@gmail.com>
2017-01-26 09:44:54 -0600

    Remove console.logs in public scripts

--- public/scripts/services/multipartForm.js
+++ public/scripts/services/multipartForm.js
@@ -1,12 +1,9 @@
 app.service('MultipartForm', ['$http', function($http) {
   this.post = function(uploadUrl, surveyId, data, idToken) {
     var fd = new FormData(); //We will store form data here to be sent to server
-    console.log("MultipartForm data: ", data);
-
     //Loop through fileInfo object and append each info property to fd
     for (var file in data.filesInfo) {
       if (data.filesInfo.hasOwnProperty(file)) {
-        console.log("MultipartForm for in loop: " + file + " -> " + data.filesInfo[file]);
         fd.append(file, data.filesInfo[file]);
       }
     }
@@ -14,7 +11,6 @@ app.service('MultipartForm', ['$http', function($http) {
     for( var i = 0; i< data.files.length ; i++ ){
         fd.append('file' , data.files[i] );
     }
-
     return $http.post(uploadUrl, fd, { //this is a configuration for the POST
       transformRequest: angular.indentity, //stops angular from serializing our data
       headers: {
