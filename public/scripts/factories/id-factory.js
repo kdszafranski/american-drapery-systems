c54845d85055f8570b1e851cf2636fcd8c7147db
Jeff Kealy <jeffkealy@yahoo.com>
2017-01-26 09:45:03 -0600

    removing logs

--- public/scripts/factories/id-factory.js
+++ public/scripts/factories/id-factory.js
@@ -12,19 +12,15 @@ app.factory("IdFactory", function($interval)
   return {
     id: id,
     setSurvey: function(newId) {
-      console.log('changing survey to ', newId);
       id.survey = newId;
     },
     setArea: function(newId) {
-      console.log('changing survey to ', newId);
       id.area = newId;
     },
     setMeasurement: function(newId) {
-      console.log('changing survey to ', newId);
       id.measurement = newId;
     },
     setClient: function(newId) {
-      console.log('changing survey to ', newId);
       id.client = newId;
     },
     getSurveyId: function() {
