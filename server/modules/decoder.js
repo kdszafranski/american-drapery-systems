2316619faf7ca292cab1f9f40fe1000dd0c2cf7e
Alex Hermundson <ahermundson@Alexs-MacBook-Air.local>
2017-01-26 21:56:35 -0600

    fixed lowercase m in firebase

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -10,7 +10,7 @@ admin.initializeApp({
     "auth_uri": process.env.FIREBASE_AUTH_URI,
     "token_uri": process.env.FIREBASE_TOKEN_URI,
     "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_x509_CERT_URL
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
   }),
   databaseURL: "https://american-drapery-systems-3a36f.firebaseio.com"
 });
