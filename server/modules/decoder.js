c6de4c3f772a79f447a98ec731bfc93f6b9b817b
Casey Hyde <hyde.casey@gmail.com>
2017-02-02 23:10:06 -0600

    update firebase info

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -12,7 +12,7 @@ admin.initializeApp({
     "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
     "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
   }),
-  databaseURL: "https://american-drapery-systems-3a36f.firebaseio.com"
+  databaseURL: "https://ads-portfolio.firebaseio.com";
 });
 
 var tokenDecoder = function(req, res, next){
