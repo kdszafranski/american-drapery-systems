2745e83458766a3f1e2d9ab8f7dba7150ccf1357
Jake Iwen <iwenx006@umn.edu>
2017-03-22 10:48:11 -0500

    firebase config

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,11 +1,11 @@
 /**********************
 FIREBASE CONFIGUARTION
 ***********************/
-  var config = {
-    apiKey: "AIzaSyBe__MNUrW2emO0-VDViZuTlTtRVUNX1ug",
-    authDomain: "american-drapery-systems-3a36f.firebaseapp.com",
-    databaseURL: "https://american-drapery-systems-3a36f.firebaseio.co",
-    storageBucket: "american-drapery-systems-3a36f.appspot.com",
-    messagingSenderId: "50273795397"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyB2yNYZp9m3LRB1YheXq64np4bITRA-SUs",
+  authDomain: "ads-prime-58cf5.firebaseapp.com",
+  databaseURL: "https://ads-prime-58cf5.firebaseio.com",
+  storageBucket: "ads-prime-58cf5.appspot.com",
+  messagingSenderId: "447384462470"
+};
+firebase.initializeApp(config);
