640443ff3bbef3021c56047923de56fc645d99f3
Jake Iwen <iwenx006@umn.edu>
2017-03-22 10:42:15 -0500

    new db vars

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,26 @@
 const url = require('url');
 
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
+// const params = url.parse(process.env.DATABASE_URL);
+// const auth = params.auth.split(':');
+//
+// const config = {
+//   user: auth[0],
+//   password: auth[1],
+//   host: params.hostname,
+//   port: params.port,
+//   database: params.pathname.split('/')[1],
+//   ssl: true
+// };
 
 const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
   ssl: true
 };
+
 
 
 module.exports = config;
