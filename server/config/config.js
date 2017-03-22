8881d08649ead5e5614c984e767a94aa576e1378
Jake Iwen <iwenx006@umn.edu>
2017-03-22 11:25:02 -0500

    heroku db

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,14 @@
 const url = require('url');
 
-// const params = url.parse(process.env.DATABASE_URL);
-// const auth = params.auth.split(':');
-//
-// const config = {
-//   user: auth[0],
-//   password: auth[1],
-//   host: params.hostname,
-//   port: params.port,
-//   database: params.pathname.split('/')[1],
-//   ssl: true
-// };
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
 
 const config = {
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
   ssl: true
 };
 
