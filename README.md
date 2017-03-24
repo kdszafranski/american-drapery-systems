b68f45690be40a1486195f8f9f5328563bb0f9f2
Jake Iwen <iwenx006@umn.edu>
2017-03-23 22:17:24 -0500

    add test data

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # American Drapery Systems Measurement Tool
 
-This application was built for American Drapery Systems to help with their measurement process.
+This application was built for American Drapery Systems to help with their window and drapery measurement process.
+
+It connects clients to respective data on building surveys done by ADS, the rooms/areas within the building and specific window measurements for each room.
+
+
 
 This application was developed by Alex Hermundson, Casey Hyde, Jake Iwen, & Jeff Kealy for the group project phase of Prime Digital Academy.
 
--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -1,20 +1,31 @@
-var admin = require("firebase-admin");
-admin.initializeApp({
-  credential: admin.credential.cert({
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
-  }),
-  databaseURL: "https://ads-portfolio.firebaseio.com"
-});
 
+var admin = require("firebase-admin");
+var fs = require("fs");
+
+fs.stat('./server/firebase-service.json', function(err, stat) {
+  if(err == null) {
+    admin.initializeApp({
+      credential: admin.credential.cert("./server/firebase-service.json"),
+      databaseURL: "https://ads-prime-58cf5firebaseio.com"
+    });
+  } else if(err.code == 'ENOENT') {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        "type": process.env.FIREBASE_TYPE,
+        "project_id": process.env.FIREBASE_PROJECT_ID,
+        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+        "client_id": process.env.FIREBASE_CLIENT_ID,
+        "auth_uri": process.env.FIREBASE_AUTH_URI,
+        "token_uri": process.env.FIREBASE_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+      }),
+      databaseURL: "https://ads-prime-58cf5.firebaseio.com"
+    });
+  }
+});
 var tokenDecoder = function(req, res, next){
   // console.log("token decoder runnning. Headers: ", req.headers);
   if (req.headers.id_token) {
@@ -32,6 +43,6 @@ var tokenDecoder = function(req, res, next){
     // Will also be hit when user does not send back an idToken in the header
     res.sendStatus(403);
   }
-}
+};
 
 module.exports = { token: tokenDecoder };
