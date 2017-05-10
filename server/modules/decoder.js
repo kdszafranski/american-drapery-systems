8d33d55ee86bc3a96fc6993607cc89b673305036
Kris Szafranski <kdszafranski@gmail.com>
2017-05-10 11:14:03 -0500

    fixed env var names

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -10,16 +10,16 @@ fs.stat('./server/firebase-service-account.json', function(err, stat) {
   } else if(err.code == 'ENOENT') {
     admin.initializeApp({
       credential: admin.credential.cert({
-        "type": process.env.FIREBASE_TYPE,
-        "project_id": process.env.FIREBASE_PROJECT_ID,
-        "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
-        "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-        "client_id": process.env.FIREBASE_CLIENT_ID,
-        "auth_uri": process.env.FIREBASE_AUTH_URI,
-        "token_uri": process.env.FIREBASE_TOKEN_URI,
-        "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+        "type": process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+        "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+        "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+        "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+        "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+        "auth_uri": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+        "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+        "auth_provider_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+        "client_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL
       }),
       databaseURL: "https://prime-american-drapery.firebaseio.com"
     });
